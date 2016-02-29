6c5be2fa6a2a6020f1f30f34d3fcda0d20d56a28
Sergei AVI-crak <Sergei AVI-crak@localhost>
2016-03-01 05:00:56 +0700

    ????? ?????

--- RtoS.h
+++ RtoS.h
@@ -18,53 +18,49 @@
 //#define _bitFlag volatile uint32_t __attribute__ ((section(".flag"))) // так лучше
 
 
-/// sSustem_task - формат банка
+/// sSystem_task - формат банка
 struct
 {
-  struct task* activ;               ///#00- Адрес активной задачи
-  struct task* delay;               ///#04- Адрес спящих задачь
-  struct task* wait;                ///#08- Адрес задач ожидающих пинка
-  struct task* hold;                ///#12- Адрес задач на обработку памяти
-  __IO uint32_t NVIC_size_max;      ///#16- использованный размер стека прерываний
-  __IO uint32_t NVIC_size;          ///#20- размер стека прерываний, задаётся при старте
-  __IO uint32_t Main_size_start;    ///#24- размер стека майна// удачный стек
+  struct task* activ;               ///#00- Адрес активной задачи, Active task pointer
+  struct task* delay;               ///#04- Адрес спящих задачь, Sleeping tsaks list pointer
+  struct task* wait;                ///#08- Адрес задач ожидающих пинка, Waiting tasks list pointer
+  struct task* hold;                ///#12- Адрес задач на обработку памяти, Tasks on hold list pointer
+  __IO uint32_t NVIC_size_max;      ///#16- Рабочий стек прерываний, Task maximum NVIC used stack
+  __IO uint32_t NVIC_size;          ///#20- Размер стека прерываний, Task NVIC stack size
+  __IO uint32_t Main_size_start;    ///#24-
+  /// Старт - Размер стека майна, Task MAIN stack size
+  /// Работа - Удачный стек, booked stack address
   __IO uint32_t sSYSHCLK;           ///#28- системная частота, гц - задается при старте
-  __IO uint32_t tik_real;           ///#32- 100% тиков на задачу // us на задачу при старте
-  __IO uint32_t malloc_start;       ///#36- первый адрес malloc
-  __IO uint32_t malloc_stop;        ///#40- последний адрес malloc
-  __IO uint32_t task_stop;          ///#44- последний адрес стека
-  __IO uint32_t alarm_mc;           ///#48- время для задержки выполнения условия
+  __IO uint32_t tick_real;           ///#32-
+  /// Старт - дительность цикла задачи (us), Start - duration of the task cycle (us)
+  /// Работа - таймер активности задачи 100%, Task activity timer 100%
+  __IO uint32_t malloc_start;       ///#36- Первый адрес malloc, First malloc address
+  __IO uint32_t malloc_stop;        ///#40- Последний адрес malloc, Last malloc address
+  __IO uint32_t task_stop;          ///#44- Последний адрес стека, Stack last address
+  __IO uint32_t alarm_mc;           ///#48- системное время, System time counter
+  __IO uint32_t sustem_us;           ///#52- временно
 
-}sSustem_task ;
-
-
-/*
-struct
-{
-    uint16_t task_nomer_f[10];
-    char* task_names_f[10];
-
-}sSustem_tas;
-//sSustem_task.activ -> task_names
-*/
+}sSystem_task ;
 
 
 
 volatile struct  task
 {
-  struct task* task_new;            /// 0x00, #00  32b,- Адрес новой задачи
-  struct task* task_lid;            /// 0x04, #04, 32b,- Адрес старой задачи
-  __IO uint32_t last_stack;         /// 0x08, #08, 32b,- Cтек задачи (активный хвост)
-  __IO uint16_t life_time;          /// 0x0C, #12, 16b,- Таймер активности в потоке
-  __IO uint8_t percent;             /// 0x0E, #14, 08b,- Выделенный процент активности потоке
-  __IO uint8_t flag;                /// 0x0F, #15, 08b,- Флаг запроса на обработку
-  __IO uint32_t sleep_wake;         /// 0x10, #16, 32b,- Время сна (мс) // адрес глобального флага пинка
-  //__IO uint32_t data_address;       /// 0x14, #20, 32b,- Malloc_zize - malloc_adres // malloc_adres
-  __IO uint16_t task_nomer;         /// 0x14, #20, 16b,- Уникальный номер таска - создаётся программно
-  __IO uint16_t task_xxxxx;         /// 0x16, #22, 16b,- Пусто
-  __IO uint16_t stack_zize;         /// 0x18, #24, 16b,- Размер стека (задаётся при запуске)
-  __IO uint16_t stack_max_zize;     /// 0x1A, #26, 16b,- Мах размер стека (мах заюзанный размер стека)
-  __IO char* task_names;            /// 0x1C, #28, 32b,- Адрес имени задачи (char* const text)
+  struct task* task_new;            /// 0x00, #00  32b,- Адрес новой задачи, New task pointer
+  struct task* task_lid;            /// 0x04, #04, 32b,- Адрес старой задачи, Old task pointer
+  __IO uint32_t last_stack;         /// 0x08, #08, 32b,- Cтек задачи, Stack pointer
+  __IO uint16_t life_time;          /// 0x0C, #12, 16b,- Таймер активности задачи, Task activity timer
+  __IO uint8_t percent;             /// 0x0E, #14, 08b,- Процент активности задачи, Task percentage usage
+  __IO uint8_t flag;                /// 0x0F, #15, 08b,- Флаг запроса на обработку, Preocessing request flag
+  __IO uint32_t task_wake;          /// 0x10, #16, 32b,
+  /// struct task* delay - Время сна (мс), Sleeping time
+  /// struct task* wait - Адрес глобального флага пинка, Address global flag kick
+  /// struct task* hold - Переменная обработки mallok, Malloc processing variable
+  __IO uint16_t task_nomer;         /// 0x14, #20, 16b,- Номер таска, Task iniqe ID
+  __IO uint16_t task_xxxxx;         /// 0x16, #22, 16b,- Пусто, Resrved
+  __IO uint16_t stack_zize;         /// 0x18, #24, 16b,- Размер стека, Task stack size
+  __IO uint16_t stack_max_zize;     /// 0x1A, #26, 16b,- Рабочий стек, Task maximum used stack
+  __IO char* task_names;            /// 0x1C, #28, 32b,- Имя задачи, Task mame
 };
 
 volatile uint32_t Random_register[3];
@@ -83,43 +79,10 @@ volatile uint32_t Random_register[3];
 
 
 маллок
-0x00, #00, 32b, 9b (номер задачи) - 23b (адрес смещения)
+0x00, #00, 32b, 9b-H (номер задачи) - 23b-L (адрес смещения)
 0x04, #04, 32b, дата (минимум 32b)
 */
-/*
-#define ITM_Port8(n)    (*((volatile unsigned char *)(0xE0000000+4*n)))
-#define ITM_Port16(n)   (*((volatile unsigned short*)(0xE0000000+4*n)))
-#define ITM_Port32(n)   (*((volatile unsigned long *)(0xE0000000+4*n)))
 
-#define DEMCR           (*((volatile unsigned long *)(0xE000EDFC)))
-#define TRCENA          0x01000000
-
-void fputc(uint32_t ch) {
- // if (DEMCR & TRCENA) {
-    while (ITM_Port32(0) == 0);
-    ITM_Port8(0) = ch;
-  }
-*/
-/*
-void sustem_data (void)
-{
-    uint32_t *tmp1;
-    uint32_t *tmp2 = 0;
-    uint32_t tmp3 = 0;
-    uint32_t tmp4;
-    tmp1 = sSustem_task.activ;
-    tmp2 = *tmp1;
-    while (!(tmp1 == tmp2))
-        {
-            tmp2 = *tmp2;
-            sSustem_tas.task_nomer_f [tmp3] = *(tmp2 +5);
-            sSustem_tas.task_names_f [tmp3] = *(tmp2 +7);
-            tmp3++;
-
-        }
-
-}
-*/
 
 /// Отдать память другой нитке ( & link_memory, "task_func_name") (функция подтверждения)
 static void sTask_memory_donate (uint32_t *link_memory, char* const task_func_name)
@@ -146,14 +109,14 @@ return malloc_adres;
 static inline sTask_resource_ask (uint32_t *name_resource)
 {
     while ( *name_resource ) {TIM6 -> CNT = TIM6 -> ARR;};
-        *name_resource = sSustem_task.activ -> task_names;
+        *name_resource = sSystem_task.activ -> task_names;
 }
 
 /// Освободить ресурс
 static void sTask_resource_free (uint32_t *name_resource);
 void sTask_resource_free (uint32_t *name_resource)
 {
-    if ( *name_resource == sSustem_task.activ -> task_names ) *name_resource = 0; else;
+    if ( *name_resource == sSystem_task.activ -> task_names ) *name_resource = 0; else;
 }
 
 /// Функция отложенного события по циклическому таймеру
@@ -166,11 +129,11 @@ static uint32_t sTask_alarm_mc(uint32_t * timer_name, uint32_t timer_mc)
 
     if ( *timer_name ==0)
     {
-        if  ((sSustem_task.alarm_mc + timer_mc) < sSustem_task.alarm_mc)
-            alarm = 0; else *timer_name = sSustem_task.alarm_mc + timer_mc;
+        if  ((sSystem_task.alarm_mc + timer_mc) < sSystem_task.alarm_mc)
+            alarm = 0; else *timer_name = sSystem_task.alarm_mc + timer_mc;
     }else
     {
-        if  (sSustem_task.alarm_mc > *timer_name)
+        if  (sSystem_task.alarm_mc > *timer_name)
            {alarm = 1; *timer_name = 0;}
             else alarm =0;
     }
@@ -183,7 +146,7 @@ return alarm;
 static uint32_t malloc (uint32_t malloc_zize);
 uint32_t malloc (uint32_t malloc_zize)
 {
-sSustem_task.activ->flag = 1;
+sSystem_task.activ->flag = 1;
 register *malloc_adres  asm ("r0") ;
 asm volatile ("mov      r0, %[malloc_zize_] \n\t"
               "svc      0xA                 \n\t"
@@ -195,13 +158,13 @@ return malloc_adres;
 static void free (void* malloc_adres);
 void free (void* malloc_adres)
 {
-    sSustem_task.activ->flag = 2;
+    sSystem_task.activ->flag = 2;
     asm volatile ("mov      r0, %[malloc_ad_]   \n\t"
                   "svc      0xA                 \n\t"
                   :: [malloc_ad_] "r" (malloc_adres): "r0","memory");   // нить отложенных заданий
 }
 
-/// sTask_wake (глобальный флаг) разбудить задачу
+/// sTask_wake (& глобальный флаг) разбудить задачу
 static inline sTask_wake(uint32_t* task_global_flag)
 {
 asm volatile  ("push   {r2}                \n\t"
@@ -212,7 +175,7 @@ asm volatile  ("push   {r2}                \n\t"
 }
 
 
-/// sTask_wait (глобальный флаг) остановить задачу в ожидание пинка
+/// sTask_wait (& глобальный флаг) остановить задачу в ожидание пинка
 static inline sTask_wait(uint32_t* task_global_flag)
 {
 asm volatile  ("push   {r2}                \n\t"
@@ -243,10 +206,15 @@ void setup_run(uint32_t __SYSHCLK,
                         uint32_t NVIC_size,
                         uint32_t task_time_us )
 {
-    sSustem_task.sSYSHCLK = __SYSHCLK;
-    sSustem_task.Main_size_start = _main_size;
-    sSustem_task.NVIC_size = NVIC_size;
-    sSustem_task.tik_real = task_time_us;
+
+
+    CoreDebug-> DEMCR |= 0x01000000;
+    DWT->CYCCNT =0;
+    DWT->CTRL |=1; // enable the counter
+    sSystem_task.sSYSHCLK = __SYSHCLK;
+    sSystem_task.Main_size_start = _main_size;
+    sSystem_task.NVIC_size = NVIC_size;
+    sSystem_task.tick_real = task_time_us;
     Start_task();
 }
 
@@ -298,7 +266,6 @@ register volatile   *task_size__      asm     ("r1") = task_size;
 register volatile   *task_time_rate__ asm     ("r2") = task_time_rate;
 register volatile   *task_func_name__ asm     ("r3") = task_func_name;
 register volatile   *func_massif4__   asm     ("r4") = task_func_massif4_data;
-
 // да чтоб икалось в бесконечности всем рацинализаторам GCC
     asm volatile  ( "push    {r4, r5, r6}   \n\t"
      "_Zd_%=:"      "svc     0x4            \n\t"  //
