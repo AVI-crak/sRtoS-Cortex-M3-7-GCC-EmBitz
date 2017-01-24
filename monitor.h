84a379163331bd36e5518acd250bfbbf9b772c75
Sergei AVI-crak <Sergei AVI-crak@localhost>
2017-01-25 04:00:29 +0600

    ???????? ?????? ???????? ??? Cortex-M7

--- monitor.h
+++ monitor.h
@@ -1,38 +1,44 @@
+/**
+ @file    monitor.h
+ @author  AVI-crak
+ @version V-50%
+ @date    25-января-2017
+ @brief   Аxis sRtoS, Cortex-M7 ARM GCC EmBitz
+
+ license
+ Million helicopters and one cent.
+
+ форум для обсуждения
+ http://forum.ixbt.com/topic.cgi?id=48:11735
+
+ репозиторий
+ https://bitbucket.org/AVI-crak/rtos-cortex-m3-gcc
+*/
 
 #ifndef _monitor_
-
-
 
 #define buf_zize    (512)
 uint8_t  _std_out_buffer[buf_zize];
 uint8_t  _std_in_buffer[buf_zize];
+volatile uint8_t m_mk_buf[buf_zize + 1];
 
-
-union m_mk_b
+struct _stdout
 {
-uint8_t     buf_8[(buf_zize + 1)];
-uint32_t    buf_32[(buf_zize >> 2)];
-}m_mk_buf;
+    const uint16_t              length;
+    const volatile  uint16_t    tail;
+    volatile uint16_t           head;
+    uint16_t                    mode;
+    volatile uint8_t            *ptr;
+}_eb_monitor_stdout ={buf_zize,0,0,1,&_std_out_buffer[0]};
 
-
-
-typedef struct
+struct _stdin
 {
-    uint16_t            length;
-    volatile uint16_t   tail;
-    volatile uint16_t   head;
-    uint8_t             *ptr;
-}_stdout,_stdin;
-extern  _stdout	_eb_monitor_stdout;
-extern  _stdin	_eb_monitor_stdin;
-
-void monitor_reset (void)
-{
-    _eb_monitor_stdout.ptr = &_std_out_buffer[0];
-    _eb_monitor_stdout.length = buf_zize ;
-    _eb_monitor_stdin.ptr = &_std_in_buffer[0];
-    _eb_monitor_stdin.length = buf_zize ;
-};
+    const uint16_t          length;
+    volatile uint16_t       tail;
+    const volatile uint16_t head;
+    uint16_t                mode;
+    const volatile uint8_t  *ptr;
+}_eb_monitor_stdin ={buf_zize,0,0,1,&_std_in_buffer[0]};
 
 
 void monitor_fining (void)
@@ -40,61 +46,58 @@ void monitor_fining (void)
     uint16_t tmp = 0;
     do
     {
-        m_mk_buf.buf_32[tmp++] = 0x20202020;
-    }while (tmp != (buf_zize >> 2));
+        m_mk_buf[tmp++] = 0x20;
+    }while (tmp != buf_zize);
 };
 
 
-
- /// РїРµС‡Р°С‚СЊ СЃС‚СЂРѕРєРё
+ /// печать строки
 void monitor_print (uint8_t* text)
 {
-    uint32_t temp_t = 0;
+    uint16_t temp_t = 0;
     if (text[temp_t] == '\0') return;
-    uint32_t temp_h = _eb_monitor_stdout.head;
-    uint32_t temp_l;
-    while (text[temp_t] != '\0')
+    uint16_t temp_h = _eb_monitor_stdout.head;
+    uint16_t temp_l;
+    do
     {
         temp_l = temp_h; temp_h++;
         if (temp_h == buf_zize) temp_h = 0;
-        if (_eb_monitor_stdout.tail != temp_h ) *(_eb_monitor_stdout.ptr + temp_h) = text[temp_t];
+        if (_eb_monitor_stdout.tail != temp_h ) _eb_monitor_stdout.ptr[temp_h] = text[temp_t++];
         else
         {
             _eb_monitor_stdout.head = temp_l;
-            while (_eb_monitor_stdout.tail != temp_l ) sTask_skip();// Delay(1000);// С‚Р°Рј Р·Р°РЅСЏС‚Рѕ
-            *(_eb_monitor_stdout.ptr + temp_h) = text[temp_t];
+            while (_eb_monitor_stdout.tail != temp_l ) sTask_skip();// Delay(1000);// там занято
+            _eb_monitor_stdout.ptr[temp_h] = text[temp_t++];
             _eb_monitor_stdout.head = temp_h;
         }
-        temp_t++;
-    }
+    }while (text[temp_t] != '\0') ;
     _eb_monitor_stdout.head = temp_h;
 };
 
 uint8_t *monitor_scan (uint8_t *text)
 {
-    uint32_t temp_h = _eb_monitor_stdin.head;
-    uint32_t temp_l = _eb_monitor_stdin.tail;
-    uint32_t temp_x = 0;
-    if(temp_l == temp_h) {*text = 0; return text;};
+    uint16_t temp_h = _eb_monitor_stdin.head;
+    uint16_t temp_l = _eb_monitor_stdin.tail;
+    uint16_t temp_x = 0;
+    if(temp_l == temp_h) { text[0] = 0; return text;};
     while (temp_l != temp_h)
     {
         temp_l++; if (temp_l == buf_zize) temp_l = 0;
-        *(text + temp_x++) = *(_eb_monitor_stdin.ptr + temp_l);
+        text[temp_x++] = _eb_monitor_stdin.ptr[temp_l];
     }
     _eb_monitor_stdin.tail = temp_l;
-    *(text + (temp_x)) = 0;
+    text[temp_x] = 0;
     return text;
 }
 
 
+
+static void monitor (void);
 void monitor (void)
 {
-    uint32_t    *Ltask_step;
-uint32_t    *Ltask;
 uint32_t    Ltask_list_zize_sys;
-uint32_t    Ltask_list_zize_use;
 uint32_t    temp_list;
-uint32_t    temp_use;
+volatile uint32_t    temp_use;
 uint16_t    temp_buf;
 uint32_t    temp_n;
                         //  5    10     16
@@ -104,94 +107,62 @@ uint32_t time_tax2;
 
 while(1)
 {
-   time_tax = sSystem_task.system_us;
 //                   15           5        10        10        10
 monitor_print("Task_name     ;  ID;     zize; max_zize;     time;   mode\n");
-Ltask_list_zize_sys = sSystem_task.task_list_zize_sys;    //РљРѕР»РёС‡РµСЃС‚РІРѕ С‚Р°СЃРєРѕРІ РІ СЃРёСЃС‚РµРјРµ
+while ( ! sSystem_task.sustem_flag.flag.reliability_task_list ) sTask_skip();
+time_tax = sSystem_task.system_us;
+Ltask_list_zize_sys = sSystem_task.task_list_zize_sys;    //Количество тасков в системе
 temp_list = 0;
 while(Ltask_list_zize_sys != 0 )
 {
-   if ( *(sSystem_task.task_list + temp_list) != 0 )
+   if ( sSystem_task.task_list[temp_list] != 0 )
    {
         monitor_fining();
-        struct task* Ltask; Ltask = *(sSystem_task.task_list + temp_list);temp_use = 0; temp_buf = 0;
+        temp_use = 0; temp_buf = 0;
         do
         {
-          m_mk_buf.buf_8[temp_buf++] = Ltask->task_names[temp_use++];
+          m_mk_buf[temp_buf++] = sSystem_task.task_list[temp_list]->task_names[temp_use++];
         }
-        while (Ltask->task_names[temp_use] != '\0');
-        temp_use = Ltask->task_nomer;temp_buf = 19;
-        while (temp_use != 0){m_mk_buf.buf_8[--temp_buf] = temp_use %10 + '0'; temp_use /= 10; };
-        temp_use = Ltask->stack_zize;temp_buf = 29;
-        while (temp_use != 0){m_mk_buf.buf_8[--temp_buf] = temp_use %10 + '0'; temp_use /= 10; };
-        temp_use = Ltask->stack_max_zize;temp_buf = 39;
-        while (temp_use != 0){m_mk_buf.buf_8[--temp_buf] = temp_use %10 + '0'; temp_use /= 10; };
-        temp_use = Ltask->life_time;temp_buf = 49;
-        while (temp_use != 0){m_mk_buf.buf_8[--temp_buf] = temp_use %10 + '0'; temp_use /= 10; };
-        temp_n = 16;
-        if (sSystem_task.hold != 0)
-        {
-            Ltask_step = sSystem_task.hold;temp_use = sSystem_task.task_list_zize_sys;
-            do
-            {
-                if (Ltask_step == Ltask) { temp_n = 0;}
-                else Ltask_step = *Ltask_step;
-            }while (( Ltask_step != sSystem_task.hold ) && temp_use--);
-        };
-        if (sSystem_task.wait != 0)
-        {
-            Ltask_step = sSystem_task.wait;temp_use = sSystem_task.task_list_zize_sys;
-            do
-            {
-                if (Ltask_step == Ltask) { temp_n = 5;}
-                else Ltask_step = *Ltask_step;
-            }while (( Ltask_step != sSystem_task.wait ) && temp_use--);
-        };
-        if (sSystem_task.delay != 0)
-        {
-            Ltask_step = sSystem_task.delay;temp_use = sSystem_task.task_list_zize_sys;
-            do
-            {
-                if (Ltask_step == Ltask) { temp_n = 10; }
-                else Ltask_step = *Ltask_step;
-            }while (( Ltask_step != sSystem_task.delay ) && temp_use--);
-        }else
-        {
-            Ltask_step = sSystem_task.activ;temp_use = sSystem_task.task_list_zize_sys;
-            do
-            {
-                if (Ltask_step == Ltask) { temp_n = 16;}
-                else Ltask_step = *Ltask_step;
-            }while (( Ltask_step != sSystem_task.activ ) && temp_use--);
-
-        };
+        while (sSystem_task.task_list[temp_list]->task_names[temp_use] != '\0');
+        temp_use = sSystem_task.task_list[temp_list]->mode_nomer.m_n.task_nomer; temp_buf = 19;
+        while (temp_use != 0){m_mk_buf[--temp_buf] = temp_use %10 + '0'; temp_use /= 10; };
+        temp_use = sSystem_task.task_list[temp_list]->stack_zize;temp_buf = 29;
+        while (temp_use != 0){m_mk_buf[--temp_buf] = temp_use %10 + '0'; temp_use /= 10; };
+        temp_use = sSystem_task.task_list[temp_list]->stack_max_zize; temp_buf = 39;
+        while (temp_use != 0){m_mk_buf[--temp_buf] = temp_use %10 + '0'; temp_use /= 10; };
+        temp_use = sSystem_task.task_list[temp_list]->life_time; temp_buf = 49;
+        while (temp_use != 0){m_mk_buf[--temp_buf] = temp_use %10 + '0'; temp_use /= 10; };
+        temp_use = sSystem_task.task_list[temp_list]->mode_nomer.m_n.mode;
+        if (temp_use == 0)temp_n = 0; //hold = 0  >0
+        if (temp_use == 1)temp_n = 5; //wait = 1  >5
+        if (temp_use == 2)temp_n = 10;  //delay = 2 >10
+        if (temp_use == 3)temp_n = 16;  //activ = 3 >16
         temp_buf = 52;
         do
         {
-          m_mk_buf.buf_8[temp_buf++] = txt_mode[temp_n++];
+          m_mk_buf[temp_buf++] = txt_mode[temp_n++];
         }
         while ( txt_mode[temp_n] != '\0');
         temp_buf = 59;
-        m_mk_buf.buf_8[temp_buf++] = '\n';
-        m_mk_buf.buf_8[temp_buf++] = 0;
-        monitor_print (&m_mk_buf.buf_8[0]);
-        temp_list++; Ltask_list_zize_sys--;
-   }else
-   {
-     temp_list++;
-     if (temp_list > sSystem_task.task_list_zize_use) break;
-   }
+        m_mk_buf[temp_buf++] = '\n';
+        m_mk_buf[temp_buf++] = 0;
+        monitor_print (&m_mk_buf[0]);
+    }
+    temp_list++; Ltask_list_zize_sys--;
 }
-
-monitor_print ("NVIC_size_max: "); monitor_print (t32_char( sSystem_task.NVIC_size_max ,txt_buf));
-monitor_print ("\nNVIC_size: "); monitor_print (t32_char( sSystem_task.NVIC_size ,txt_buf));
-monitor_print ("\nTime_ptint: ");monitor_print (t32_char(time_tax2,txt_buf));
+uint8_t shaize[12];
+monitor_print ("NVIC_size_max: "); monitor_print (_t32_char( sSystem_task.NVIC_size_max,&shaize));
+monitor_print ("\nNVIC_size: "); monitor_print (_t32_char( sSystem_task.NVIC_size,&shaize));
+monitor_print ("\nTime_ptint: "); monitor_print (_t32_char(time_tax2,&shaize));
 time_tax2 = sSystem_task.system_us - time_tax;
-if (time_tax2 < 1500)temp_use = 1500;else temp_use = time_tax2 * 5;
+if (6000 < time_tax2 < 1000)temp_use = 3000;else temp_use = time_tax2;
+monitor_print ("ms\n\t");
 sDelay_mc(temp_use);
 monitor_print("\f");
 
 }
+
+
 }
 
 
